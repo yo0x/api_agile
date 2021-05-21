1a62e6e018224d793c33e924c1f7790672b35493
yo0x <6yonor@gmail.com>
2021-05-21 13:42:11 +0000

    start

---
+++ api.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+data = {
+      "accountname" : "משה",
+     "telephone1" : "036339060",
+     "idnumber" : "1234",
+     "billingcity" : "תל אביב"
+}
+
+url = 'https://api.powerlink.co.il/api/record/account'
+token_id = 'xxxxxxx-xxxxx-xxxxx-xxxxx'
+headers = {'Content-type': 'application/json', 'tokenId': token_id}
+response = requests.post(url, data=str(data), headers=headers)
+return json.loads(response.content)['data']['Record']
